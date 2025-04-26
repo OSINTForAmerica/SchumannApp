b4d93fd1e5221cd7bdab24b05da1e85de4a906ba
OSINTForAmerica <158000811+OSINTForAmerica@users.noreply.github.com>
2025-04-26 17:26:46 -0400

    Update service-worker.js
    tweaks

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,27 @@
-const CACHE_NAME = 'tomsk-schumann-cache-v1';
-const URLS_TO_CACHE = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/manifest.json',
-  '/icon-192.png',
-  '/icon-512.png'
-];
-
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(URLS_TO_CACHE))
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('schumann-cache').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/icon-512.png',
+        '/icon-192.png',
+        '/manifest.json',
+        'https://cdn.tailwindcss.com',
+        'https://unpkg.com/dayjs/dayjs.min.js',
+        'https://unpkg.com/dayjs/plugin/utc.js',
+        'https://unpkg.com/dayjs/plugin/timezone.js',
+        'https://cdn.jsdelivr.net/npm/glightbox/dist/css/glightbox.min.css',
+        'https://cdn.jsdelivr.net/npm/glightbox/dist/js/glightbox.min.js'
+      ]);
+    })
   );
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then((response) => response || fetch(event.request))
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cachedResponse) => {
+      return cachedResponse || fetch(e.request);
+    })
   );
 });
